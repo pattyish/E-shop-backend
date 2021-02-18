c688e206a5b66069931a5d1e66bfc3d7b3618bd4
pattyish <patrickishimwe10@gmail.com>
2021-02-19 00:05:17 +0200

    get user and get all users endpoint

--- routers/users.js
+++ routers/users.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
 // Getting all Users
 router.get("/", async (req, res) => {
-  const userList = await User.find();
+  const userList = await User.find().select("-passwordHash");
   if (!userList) {
     res.status(500).json({
       status: 500,
       success: false,
+      message: "There Is No Users Yet!!",
     });
   }
   res.status(200).json({
@@ -18,7 +19,22 @@ router.get("/", async (req, res) => {
     users: userList,
   });
 });
-
+// Getting User by Id
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
+  if (!user) {
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "User with This Id Is Not Found!!",
+    });
+  }
+  res.status(200).json({
+    status: 200,
+    success: true,
+    user: user,
+  });
+});
 // Creating New User
 router.post("/", async (req, res) => {
   let user = new User({
@@ -39,12 +55,13 @@ router.post("/", async (req, res) => {
     return res.status(404).json({
       status: 404,
       success: false,
-      message: "The user Not Modified!!",
+      message: "The user Not Created!",
     });
   }
   res.status(201).json({
     status: 201,
     success: true,
+    message: "User Created Successful!!",
     user: user,
   });
 });
