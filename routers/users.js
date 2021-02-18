4a2ec09772aee47480a17cd02a9cd0dd6b19edf0
pattyish <patrickishimwe10@gmail.com>
2021-02-18 23:46:19 +0200

    register endpoint

--- routers/users.js
+++ routers/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user");
 
+// Getting all Users
 router.get("/", async (req, res) => {
   const userList = await User.find();
   if (!userList) {
@@ -14,6 +15,36 @@ router.get("/", async (req, res) => {
     status: 200,
     success: true,
     users: userList,
+  });
+});
+
+// Creating New User
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+
+  user = await user.save();
+  if (!user) {
+    return res.status(404).json({
+      status: 404,
+      success: false,
+      message: "The user Not Modified!!",
+    });
+  }
+  res.status(201).json({
+    status: 201,
+    success: true,
+    user: user,
   });
 });
 
