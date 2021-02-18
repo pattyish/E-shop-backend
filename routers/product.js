5bb612b2db0dfa7e4a2e6a5a816929d18a22042b
pattyish <patrickishimwe10@gmail.com>
2021-02-18 10:00:32 +0200

    add new product in stock

--- routers/product.js
+++ routers/product.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Category } = require("../models/category");
 const router = express.Router();
 const { Product } = require("../models/product");
 
@@ -17,23 +18,40 @@ router.get("/", (req, res) => {
     );
 });
 
-router.post("/", (req, res) => {
-  const product = new Product({
+router.post("/", async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category)
+    return res.status(400).json({
+      status: 400,
+      success: false,
+      message: "Invalid Category",
+    });
+
+  let product = new Product({
     name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
     image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
     countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   });
-  product
-    .save()
-    .then((createProduct) => {
-      res.status(201).json(createProduct);
-    })
-    .catch((err) =>
-      res.status(500).json({
-        error: err,
-        success: false,
-      })
-    );
+
+  product = await product.save();
+  if (!product)
+    return res.status(500).json({
+      error: err,
+      success: false,
+    });
+  res.status(201).json({
+    status: 201,
+    success: true,
+    product: product,
+  });
 });
 
 module.exports = router;
